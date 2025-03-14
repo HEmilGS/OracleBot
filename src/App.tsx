570c5c233c01cdc24817685b66054639de97d429
HEmilGS <hectoremil748@gmail.com>
2025-03-14 01:44:34 -0600

    focus mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import SideBar from './Components/SideBar'
 import Header from './Components/header'
+import FocusMode from './pages/FocusMode'
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' necesario despues para el header
 
 
@@ -8,10 +9,13 @@ import './App.css'
 
 function App() {
   return (
-    <div className="flex bg-[#]">
+    <div className="flex bg-[#F9F8F8]">
       <SideBar />
       <div className="w-4/5">
         <Header />
+        <div className="p-4">
+          <FocusMode/>
+        </div>
       </div>
     </div>
   )

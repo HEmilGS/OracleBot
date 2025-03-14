fffec34107ba8d1b16788910128a4aa38374b821
Luis Daniel Garcia Espinosa <danny030212@gmail.com>
2025-03-14 02:11:54 -0600

    Create Task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { useState } from 'react'
-import SideBar from './Components/SideBar'
-import Header from './Components/header'
+import { useState } from "react";
+import SideBar from "./Components/SideBar";
+import Header from "./Components/header";
+import CreateTask from "./pages/CreateTask";
+
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' necesario despues para el header
 
-
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
-    <div className="flex bg-[#]">
+    <div className="flex bg-[#F9F8F8]">
       <SideBar />
       <div className="w-4/5">
         <Header />
+        <div className="p-4">
+          <CreateTask />
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-
-export default App
+export default App;

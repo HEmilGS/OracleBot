b2cb9ee7a29dc8342f8394c7b03a52de3fc52ba7
HEmilGS <hectoremil748@gmail.com>
2025-05-30 09:08:29 -0600

    ajuste de labeln't

--- MtdrSpring/backend/src/main/frontend/src/pages/Dashboard.tsx
+++ MtdrSpring/backend/src/main/frontend/src/pages/Dashboard.tsx
@@ -73,7 +73,7 @@ export default function Dashboard() {
         <MetricCard color="bg-orange-100" title="Progreso" value={metrics.enProgreso} />
         <MetricCard color="bg-red-100" title="Pendiente" value={metrics.pendientes} />
         <MetricCard color="bg-blue-100" title="Total" value={metrics.total} />
-        <MetricCard color="bg-green-100" title="Componente corregido" value={0}/>
+        <MetricCard color="bg-green-100" title="Componente incorrecto" value={0}/>
       </div>
 
       <div className="grid grid-cols-1 gap-6 lg:grid-cols-3">
